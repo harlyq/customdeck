08fe4520dbd43df68aaa2bb3597b39ea46848d2c
harlyq <celerity252@gmail.com>
2014-09-19 18:46:01 -0400

    cardLayout webcomponent
    - basic component, children's position will be set according to the
    layout attributes in the element

--- src/cardlayout.ts
+++ src/cardlayout.ts
@@ -1,41 +1,30 @@
 /*export*/
 class CardLayout {
     private _func: {
-        [style: number]: (index: number, count: number, options: CardLayout.Options) => number
+        [layout: string]: (index: number, count: number, options: CardLayout.Options) => number
     } = {};
     private options: CardLayout.Options = {};
     private transformKeyword: string = '';
 
     constructor(options: CardLayout.Options) {
         var thisOptions = this.options;
-        thisOptions.style = CardLayout.Style.Stack;
-        thisOptions.maxSpacing = 1;
-        thisOptions.align = CardLayout.Align.Center;
+        thisOptions.layout = 'left';
+        thisOptions.maxspacing = 1;
+        thisOptions.align = 'center';
         thisOptions.rotate = 0;
-        thisOptions.offsetX = 0;
-        thisOptions.offsetY = 0;
+        thisOptions.offsetx = 0;
+        thisOptions.offsety = 0;
         thisOptions.element = null;
         thisOptions.top = 0;
         thisOptions.left = 0;
         thisOptions.width = 1;
         thisOptions.height = 1;
-        thisOptions.cardWidth = 20;
-        thisOptions.cardHeight = 20;
 
-        var element = options.element;
-        if (element) {
-            thisOptions.width = element.offsetWidth;
-            thisOptions.height = element.offsetHeight;
-            thisOptions.left = element.offsetLeft;
-            thisOptions.top = element.offsetTop;
-        }
+        this.setOptions(options);
 
-        for (var i in options)
-            this.options[i] = options[i];
-
-        this.define(CardLayout.Style.Fan, CardLayout.getFan);
-        this.define(CardLayout.Style.Stack, CardLayout.getStack);
-        this.define(CardLayout.Style.Random, CardLayout.getRandom);
+        this.define('fan', CardLayout.getFan);
+        this.define('stack', CardLayout.getStack);
+        this.define('random', CardLayout.getRandom);
 
         var elem = document.createElement('div');
         var transformStrings = ['transform', 'OTransform', 'webkitTransform', 'MozTransform', 'msTransform'];
@@ -48,10 +37,25 @@ class CardLayout {
         this.transformKeyword = 'webkitTransform';
     }
 
+    setOptions(options: CardLayout.Options) {
+        var thisOptions = this.options;
+        var element = options.element;
+
+        if (element) {
+            thisOptions.width = element.offsetWidth;
+            thisOptions.height = element.offsetHeight;
+            thisOptions.left = element.offsetLeft;
+            thisOptions.top = element.offsetTop;
+        }
+
+        for (var i in options)
+            thisOptions[i] = options[i];
+    }
+
     // func returns a number in the range 0 (left-most) to 1 (right-most)
-    define(style: number,
+    define(layout: string,
         func: (index: number, count: number, options: CardLayout.Options) => number) {
-        this._func[style] = func;
+        this._func[layout] = func;
     }
 
     position(cards ? : any) {
@@ -79,8 +83,8 @@ class CardLayout {
         var numCards = cards.length;
         for (var i = 0; i < numCards; ++i) {
             var card = cards[i];
-            var left = this.getLeft(i, numCards, card.offsetWidth) - options.left;
-            var top = this.getTop(i, numCards, card.offsetHeight) - options.top;
+            var left = this.getLeft(i, numCards, card.offsetWidth); // - options.left;
+            var top = this.getTop(i, numCards, card.offsetHeight); // - options.top;
 
             card.style.position = 'absolute';
             card.style[this.transformKeyword] = 'translate(' + left + 'px,' + top + 'px)';
@@ -91,37 +95,31 @@ class CardLayout {
         return 0;
     }
 
-    getLeft(index: number, count: number, cardWidth ? : number) {
+    getLeft(index: number, count: number, cardwidth: number) {
         var options = this.options;
-        if (typeof cardWidth === 'undefined')
-            cardWidth = options.cardWidth;
-
-        return (this.getX(index, count) + 1) * (options.width - cardWidth) / 2 + index * options.offsetX + options.left;
+        return (this.getX(index, count) + 1) * (options.width - cardwidth) / 2 + index * options.offsetx; // + options.left;
     }
 
-    getTop(index: number, count: number, cardHeight ? : number) {
+    getTop(index: number, count: number, cardheight: number) {
         var options = this.options;
-        if (typeof cardHeight === 'undefined')
-            cardHeight = options.cardHeight;
-
-        return (this.getY(index, count) + 1) * (options.height - cardHeight) / 2 + index * options.offsetY + options.top;
+        return (this.getY(index, count) + 1) * (options.height - cardheight) / 2 + index * options.offsety; // + options.top;
     }
 
-    getInverseTopLeft(top: number, left: number, count: number, cardWidth: number, layoutLeft: number, layoutTop: number, layoutWidth: number, layoutHeight: number): number {
+    getInverseTopLeft(top: number, left: number, count: number, cardwidth: number, layoutLeft: number, layoutTop: number, layoutWidth: number, layoutHeight: number): number {
         return 0;
     }
 
     getX(index: number, count: number): number {
         var options = this.options;
         var rad = options.rotate / 180 * Math.PI;
-        var r = this._func[options.style](index, count, options);
+        var r = this._func[options.layout](index, count, options);
         return r * Math.cos(rad);
     }
 
     getY(index: number, count: number): number {
         var options = this.options;
         var rad = options.rotate / 180 * Math.PI;
-        var r = this._func[options.style](index, count, options);
+        var r = this._func[options.layout](index, count, options);
         return r * Math.sin(rad);
     }
 
@@ -131,29 +129,29 @@ class CardLayout {
 
     static getStack(index, count, options): number {
         switch (options.align) {
-            case CardLayout.Align.Left:
+            case 'left':
                 return -1;
                 break;
-            case CardLayout.Align.Center:
+            case 'center':
                 return 0;
                 break;
-            case CardLayout.Align.Right:
+            case 'right':
                 return 1;
                 break;
         }
     }
 
     static getFan(index, count, options): number {
-        var delta = Math.min(options.maxSpacing, (count > 1 ? 2 / (count - 1) : 0));
+        var delta = Math.min(options.maxspacing, (count > 1 ? 2 / (count - 1) : 0));
 
         switch (options.align) {
-            case CardLayout.Align.Left:
+            case 'left':
                 return index * delta;
                 break;
-            case CardLayout.Align.Center:
+            case 'center':
                 return (index - (count - 1) / 2) * delta;
                 break;
-            case CardLayout.Align.Right:
+            case 'right':
                 return 1 - (count - 1 - index) * delta;
                 break;
         }
@@ -166,28 +164,19 @@ class CardLayout {
 
 /*export*/
 module CardLayout {
+    // all lowercase as these can be set directly as options
     export interface Options {
-        style ? : Style;
-        maxSpacing ? : number; // (0,1) used by Fan
-        align ? : Align;
+        layout ? : string; // stack, fan, random
+        maxspacing ? : number; // (0,1) used by fan
+        align ? : string; // left, center, right
         rotate ? : number;
-        offsetX ? : number;
-        offsetY ? : number;
+        offsetx ? : number;
+        offsety ? : number;
         element ? : HTMLElement;
         top ? : number;
         left ? : number;
         width ? : number;
         height ? : number;
-        cardWidth ? : number;
-        cardHeight ? : number;
-    }
-
-    export enum Style {
-        Stack, Fan, Random
-    }
-
-    export enum Align {
-        Left, Center, Right
     }
 
     interface TopLeft {
@@ -199,4 +188,73 @@ module CardLayout {
         x: number;
         y: number;
     }
+}
+
+var CardLayoutPrototype = Object.create(HTMLElement.prototype);
+
+CardLayoutPrototype.createdCallback = function() {
+    this.options = {
+        element: this
+    };
+
+    var self = this;
+    [].forEach.call(this.attributes, function(attr) {
+        self.options[attr.name] = attr.value;
+    });
+};
+
+CardLayoutPrototype.attachedCallback = function() {
+    this.cardLayout = new CardLayout(this.options);
+};
+
+CardLayoutPrototype.detachedCallback = function() {
+    this.cardLayout = null;
+}
+
+CardLayoutPrototype.attributeChangedCallback = function(attrName, oldVal, newVal) {
+    this.options[attrName] = newVal;
+
+    if (this.cardLayout) {
+        this.cardLayout.setOptions(this.options);
+        this.cardLayout.position();
+    }
+}
+
+CardLayoutPrototype._position = function(cards ? : any) {
+    if (this.cardLayout)
+        this.cardLayout.position(cards);
+}
+
+CardLayoutPrototype.appendChild = function(newElement: Node) {
+    HTMLElement.prototype.appendChild.call(this, newElement);
+    if (this.cardLayout)
+        this.cardLayout.position();
+}
+
+CardLayoutPrototype.removeChild = function(oldElement: Node) {
+    HTMLElement.prototype.removeChild.call(this, oldElement);
+    if (this.cardLayout)
+        this.cardLayout.position();
+}
+
+CardLayoutPrototype.insertBefore = function(newElement: Node, referenceElement: Node) {
+    HTMLElement.prototype.insertBefore.call(this, newElement, referenceElement);
+    if (this.cardLayout)
+        this.cardLayout.position();
+}
+
+CardLayoutPrototype.replaceChild = function(newElement: Node, oldElement: Node) {
+    HTMLElement.prototype.replaceChild.call(this, newElement, oldElement);
+    if (this.cardLayout)
+        this.cardLayout.position();
+}
+
+interface Document {
+    registerElement(elementName: string, options: any);
+}
+
+if ('registerElement' in document) {
+    document.registerElement('card-layout', {
+        prototype: CardLayoutPrototype
+    });
 }
